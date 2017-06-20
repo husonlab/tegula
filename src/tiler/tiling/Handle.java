d6924c3e5e050944c3608a92596b571f05c0fdae
Ruediger <rkbzeller@gmail.com>
2017-06-20 17:19:37 +0200

    handler updated

---
+++ src/tiler/tiling/Handle.java
@@ -0,0 +1,28 @@
+package tiler.tiling;
+import javafx.scene.Group;
+import javafx.scene.shape.Shape;
+
+import java.util.BitSet;
+
+/**
+ * Created by Ruediger on 2017.06.20.
+ */
+public class Handle extends Group {
+    private Shape shape;
+    private BitSet bitSet;
+    private int type;
+    private int flag;
+
+    public void setTransX(double dx){shape.setTranslateX(dx);}
+    public void setTransY(double dy){shape.setTranslateY(dy);}
+    public double getTransX(){return shape.getTranslateX();}
+    public double getTransY(){return shape.getTranslateY();}
+    public void setBitSet(BitSet bits){this.bitSet = bits;}
+    public BitSet getBitSet(){return bitSet;}
+    public void setShape(Shape s) {this.shape = s;}
+    public Shape getShape(){return shape;}
+    public void setType(int t){this.type = t;}
+    public int getType(){return this.type;}
+    public void setFlag(int a){this.flag = a;}
+    public int getFlag(){return this.flag;}
+}
