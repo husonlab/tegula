e60339c863d3b7b5debf137b27db37d017906523
rkbzeller <rkbzeller@gmail.com>
2019-05-07 12:31:12 +0200

    Spherical case repaired: no overlaps

--- src/tegula/tiling/parts/OctTree.java
+++ src/tegula/tiling/parts/OctTree.java
@@ -58,6 +58,7 @@ public class OctTree {
      * @return true, if inserted
      */
     public boolean insert(Geometry geom, Point3D point, double tolerance) {
+        double tol = tolerance * 0.1;
         if (root == null) {
             root = new Node(point);
             size++;
@@ -66,42 +67,42 @@ public class OctTree {
         Node h = root;
         while (h != null) {
             if (Tools.distance(geom, point, h.a) > tolerance) {
-                if (point.getX() >= h.a.getX() && point.getY() >= h.a.getY() && point.getZ() >= h.a.getZ()) {
+                if (point.getX() >= h.a.getX()-tol && point.getY() >= h.a.getY()-tol && point.getZ() >= h.a.getZ()-tol) {
                     if (h.ppp == null) {
                         h.ppp = new Node(point);
                         h = null;
                     } else h = h.ppp;
-                } else if (point.getX() < h.a.getX() && point.getY() >= h.a.getY() && point.getZ() >= h.a.getZ()) {
+                } else if (point.getX() < h.a.getX()+tol && point.getY() >= h.a.getY()-tol && point.getZ() >= h.a.getZ()-tol) {
                     if (h.mpp == null) {
                         h.mpp = new Node(point);
                         h = null;
                     } else h = h.mpp;
-                } else if (point.getX() >= h.a.getX() && point.getY() < h.a.getY() && point.getZ() >= h.a.getZ()) {
+                } else if (point.getX() >= h.a.getX()-tol && point.getY() < h.a.getY()+tol && point.getZ() >= h.a.getZ()-tol) {
                     if (h.pmp == null) {
                         h.pmp = new Node(point);
                         h = null;
                     } else h = h.pmp;
-                } else if (point.getX() >= h.a.getX() && point.getY() >= h.a.getY() && point.getZ() < h.a.getZ()) {
+                } else if (point.getX() >= h.a.getX()-tol && point.getY() >= h.a.getY()-tol && point.getZ() < h.a.getZ()+tol) {
                     if (h.ppm == null) {
                         h.ppm = new Node(point);
                         h = null;
                     } else h = h.ppm;
-                } else if (point.getX() < h.a.getX() && point.getY() < h.a.getY() && point.getZ() >= h.a.getZ()) {
+                } else if (point.getX() < h.a.getX()+tol && point.getY() < h.a.getY()+tol && point.getZ() >= h.a.getZ()-tol) {
                     if (h.mmp == null) {
                         h.mmp = new Node(point);
                         h = null;
                     } else h = h.mmp;
-                } else if (point.getX() < h.a.getX() && point.getY() >= h.a.getY() && point.getZ() < h.a.getZ()) {
+                } else if (point.getX() < h.a.getX()+tol && point.getY() >= h.a.getY()-tol && point.getZ() < h.a.getZ()+tol) {
                     if (h.mpm == null) {
                         h.mpm = new Node(point);
                         h = null;
                     } else h = h.mpm;
-                } else if (point.getX() >= h.a.getX() && point.getY() < h.a.getY() && point.getZ() < h.a.getZ()) {
+                } else if (point.getX() >= h.a.getX()-tol && point.getY() < h.a.getY()+tol && point.getZ() < h.a.getZ()+tol) {
                     if (h.pmm == null) {
                         h.pmm = new Node(point);
                         h = null;
                     } else h = h.pmm;
-                } else if (point.getX() < h.a.getX() && point.getY() < h.a.getY() && point.getZ() < h.a.getZ()) {
+                } else if (point.getX() < h.a.getX()+tol && point.getY() < h.a.getY()+tol && point.getZ() < h.a.getZ()+tol) {
                     if (h.mmm == null) {
                         h.mmm = new Node(point);
                         h = null;
